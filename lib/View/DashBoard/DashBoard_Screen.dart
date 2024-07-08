79162a17f4ad26748d0799cef85efb33cd205ff6
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-07-08 15:06:29 +0500

    1.Ui Designing

--- lib/View/DashBoard/DashBoard_Screen.dart
+++ lib/View/DashBoard/DashBoard_Screen.dart
@@ -51,27 +51,27 @@ class _DashboardScreenState extends State<DashboardScreen> {
         screens: _buildScreens(),
         items: _navBarsItems(),
         confineInSafeArea: true,
-        backgroundColor: AppColors.blackColor, // Default is Colors.white.
-        handleAndroidBackButtonPress: true, // Default is true.
-        resizeToAvoidBottomInset: true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
-        stateManagement: true, // Default is true.
-        hideNavigationBarWhenKeyboardShows: true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+        backgroundColor: AppColors.blackColor,
+        handleAndroidBackButtonPress: true,
+        resizeToAvoidBottomInset: true,
+        stateManagement: true,
+        hideNavigationBarWhenKeyboardShows: true,
         decoration: NavBarDecoration(
           borderRadius: BorderRadius.circular(10.0),
           colorBehindNavBar: Colors.white,
         ),
         popAllScreensOnTapOfSelectedTab: true,
         popActionScreens: PopActionScreensType.all,
-        itemAnimationProperties:const ItemAnimationProperties( // Navigation Bar's items animation properties.
+        itemAnimationProperties:const ItemAnimationProperties(
           duration: Duration(milliseconds: 200),
           curve: Curves.ease,
         ),
-        screenTransitionAnimation:const ScreenTransitionAnimation( // Screen transition animation on change of selected tab.
+        screenTransitionAnimation:const ScreenTransitionAnimation(
           animateTabTransition: true,
           curve: Curves.ease,
           duration: Duration(milliseconds: 200),
         ),
-        navBarStyle: NavBarStyle.style1, // Choose the nav bar style with this property.
+        navBarStyle: NavBarStyle.style1,
       ),
     );
 
